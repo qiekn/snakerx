85563ab6859785ac3e4c017c6483614d17eada55
qiekn <qieknxxx@gmail.com>
2025-05-16 16:21:42 +0800

    init

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+  t.window.title = "game"
+  t.window.display = 2
+  t.version = "11.3"
+  t.window.width = 960
+  t.window.height = 540
+  t.window.vsync = 1
+  t.window.msaa = 0
+end
